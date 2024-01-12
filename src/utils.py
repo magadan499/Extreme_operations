7d4061a95dc1157e01a4cb6836f1a4a0157c151b
Maksim Toguzov <magadan499@mail.ru>
2024-01-12 19:04:33 +0300

    fix utils successful_operations

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,12 @@ def get_user_operations():
     return save_user_operations
 
 
+def successful_operations(files):
+    """Проверят операции пользователя на успешность"""
+    success_operation = []
+    for file in files:
+        if file == {}:
+            continue
+        elif file['state'] == "EXECUTED":
+            success_operation.append(file)
+    return success_operation
